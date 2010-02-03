6a6e92c6e751665fff913bdfbd10c79cc0c8d65d
Shane Graber <sgraber@gmail.com>
2010-02-02 23:52:43 -0500

    removed reinstall=false as it was causing installation problems

--- aaolm/portlet/advertising/Extensions/Install.py
+++ aaolm/portlet/advertising/Extensions/Install.py
@@ -1,4 +1,4 @@
-﻿from zope.component import getUtility
+from zope.component import getUtility
 from zope.component import getMultiAdapter
 from plone.portlets.interfaces import IPortletAssignmentMapping
 from plone.portlets.interfaces import IPortletManager
@@ -7,13 +7,14 @@ from StringIO import StringIO
 
     
 def install(portal):
+    """Install portlet, skins, and viewlets"""
     setup_tool = getToolByName(portal, 'portal_setup')
     setup_tool.runAllImportStepsFromProfile('profile-aaolm.portlet.advertising:default')
     return "Ran all import steps."
 
 
-def uninstall(portal, reinstall=False):
-    'Uninstall portal_controlpanel configlet'
+def uninstall(portal):
+    """Uninstall portlets from slots, skins layer, and viewlets."""
     
     out = []
     cp = getToolByName(portal, 'portal_controlpanel', None)
@@ -23,24 +24,23 @@ def uninstall(portal, reinstall=False):
     setup_tool.runAllImportStepsFromProfile('profile-aaolm.portlet.advertising:uninstall')
     out.append('Ran runAllImportStepsFromProfile.')
 
-    if not reinstall:
-        # Remove installed portlets from columns
-        id_portlets = ('advertisingportlet',
-                       )
-                       
-        rightcol = getUtility(IPortletManager, name=u'plone.rightcolumn', context=portal)
-        right = getMultiAdapter((portal, rightcol,), IPortletAssignmentMapping, context=portal)
-        leftcol = getUtility(IPortletManager, name=u'plone.leftcolumn', context=portal)
-        left = getMultiAdapter((portal, leftcol,), IPortletAssignmentMapping, context=portal)
-        
-        for portlet in id_portlets:
-            if portlet in right:
-                del right[portlet]
-                out.append('Removed %s portlet from right column.' % portlet) 
-            if portlet in left:
-                del left[portlet]
-                out.append('Removed %s portlet from left column.' % portlet)
-                
-        out.append('Ran all uninstall steps.')
+    # Remove installed portlets from columns
+    id_portlets = ('advertisingportlet',
+                   )
+                   
+    rightcol = getUtility(IPortletManager, name=u'plone.rightcolumn', context=portal)
+    right = getMultiAdapter((portal, rightcol,), IPortletAssignmentMapping, context=portal)
+    leftcol = getUtility(IPortletManager, name=u'plone.leftcolumn', context=portal)
+    left = getMultiAdapter((portal, leftcol,), IPortletAssignmentMapping, context=portal)
+    
+    for portlet in id_portlets:
+        if portlet in right:
+            del right[portlet]
+            out.append('Removed %s portlet from right column.' % portlet) 
+        if portlet in left:
+            del left[portlet]
+            out.append('Removed %s portlet from left column.' % portlet)
+            
+    out.append('Ran all uninstall steps.')
     
     return "\n".join(out)
